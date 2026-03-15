ffec957b0c54d6e1ad51bf414ded203256f8bba3
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2026-03-14 21:59:33 -0300

    added glfw and basic window handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use glfw::{WindowEvent, WindowMode, flush_messages};
 
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+    let mut glfw_context = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw_context.create_window(
+        1280,
+        720, 
+        "Voxploration", 
+        WindowMode::Windowed
+    ).unwrap();
+
+    window.set_close_polling(true);
+
+    'running: loop {
+        glfw_context.poll_events();
+        for (_, e) in flush_messages(&events) {
+            match e {
+                WindowEvent::Close => break 'running,
+                _ => ()
+            }
+        }
+    }
 }
 
